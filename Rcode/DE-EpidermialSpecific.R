e40e9e82222c4ab419a81b034eb9f9763df3ad2a
pengfei-qiao <36996698+pengfei-qiao@users.noreply.github.com>
2018-08-28 10:38:58 -0400

    Initial commit

--- Rcode/DE-EpidermialSpecific.R
+++ Rcode/DE-EpidermialSpecific.R
@@ -1,3 +1,6 @@
+### Author: Pengfei Qiao (pq26@cornell.edu)
+### Script to call differentially expressed genes in the paper
+
 setwd("/Users/HomeFolder/Desktop/My Computer/Labs/Scanlon Lab/Cuticle/RNAseq/analysis/version3_final")
 counts <- read.delim("counts.txt", header=T, row.names = 1)
 
